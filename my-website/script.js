da935e6ebef696423b0d4d800987331f2d08fd7b
Joonhyun <70801548+joonhyunheo9609@users.noreply.github.com>
2025-05-29 11:07:25 +0900

    adding basic js query select all to clickable

--- my-website/script.js
+++ my-website/script.js
@@ -1,1 +1,22 @@
-document.addEventListener('DOMContentLoaded')
+document.addEventListener('DOMContentLoaded', function() {
+    const sections = document.querySelectorAll('.section');
+
+    sections.forEach(function(section) {
+        section.addEventListener('click', function() {
+            if (section.classList.contains('fashion')) {
+                window.location.href = 'fashion.html';
+            }
+            else if (section.classList.contains('picture')) {
+                window.location.href = 'picture.html';
+            }
+            else if (section.classList.contains('vinyl')) {
+                window.location.href = 'vinyl.html';
+            }
+            else if (section.classList.contains('trip')) {
+                window.location.href = 'trip.html';
+            }
+        });
+        section.style.cursor = 'pointer';
+    });
+});
+
