cdee5247db00d9bc495924293705a039b23d3c73
Joonhyun <70801548+joonhyunheo9609@users.noreply.github.com>
2025-06-25 15:54:27 +0900

    adjust to back due to my js file

--- my-website/js/main.js
+++ my-website/js/main.js
@@ -4,16 +4,16 @@ document.addEventListener('DOMContentLoaded', function() {
     sections.forEach(function(section) {
         section.addEventListener('click', function() {
             if (section.classList.contains('portfolio')) {
-                window.location.href = 'pages/portfolio/portfolio.html';
+                window.location.href = 'pages/portfolio.html';
             }
             else if (section.classList.contains('fashion')) {
-                window.location.href = 'pages/fashion/fashion.html';
+                window.location.href = 'pages/fashion.html';
             }
             else if (section.classList.contains('music')) {
-                window.location.href = 'pages/music/music.html';
+                window.location.href = 'pages/music.html';
             }
             else if (section.classList.contains('trip')) {
-                window.location.href = 'pages/trip/trip.html';
+                window.location.href = 'pages/trip.html';
             }
         });
         section.style.cursor = 'pointer';
